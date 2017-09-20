5c119067cfd6b1a56429b2004352cdcf955b8042
Mr.Viana <rafaelgov95@gmail.com>
2017-09-19 21:12:33 -0400

    up

--- Spark/API/src/main/java/Main.java
+++ Spark/API/src/main/java/Main.java
@@ -1,10 +1,8 @@
 
+
 import static spark.Spark.*;
 
 public class Main {
     public static void main(String[] args) {
         port(8081); // Spark will run on port 8080
-        externalStaticFileLocation("/home/rafael/Documentos/EWII/Spark/front-end/public/");
-
-    }
-}
+        staticFileLocation("/public");    }}
